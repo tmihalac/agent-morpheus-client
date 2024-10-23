2fbd34f09968745f2849ffa0a740dd1b62152a3b
Ruben Romero Montes <rromerom@redhat.com>
2024-10-23 10:42:03 +0200

    feat: show UNKNOWN status in grey (#17)
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- src/main/webui/src/components/JustificationBanner.jsx
+++ src/main/webui/src/components/JustificationBanner.jsx
@@ -4,5 +4,8 @@ export default function JustificationBanner ({ justification }) {
   if (justification.status === "FALSE") {
     return <Label color="green">{justification.label}</Label>
   }
+  if (justification.status === "UNKNOWN") {
+    return <Label color="grey">{justification.label}</Label>
+  }
   return <Label color="red">{justification.label}</Label>
 }
